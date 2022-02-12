6a1a68298ce00043be0de4975d06c0e5d645eda5
USIS019720 <davidernestocernaavalos848@gmail.com>
2022-02-11 18:31:44 -0600

    Se termino la mayor parte del proyecto

--- Primer Proyecto/main.js
+++ Primer Proyecto/main.js
@@ -5,10 +5,10 @@ if(!db_registro){
 var app = new Vue({
     el: '#appAlumno',
     data: {
-        alumno: [],
+        alumnos: [],
         buscar: '',
         alumno: {
-            accion: '',
+            accion: 'nuevo',
             msg: '',
             idAlumno: '',
             codigo: '',
@@ -23,53 +23,56 @@ var app = new Vue({
     },
     methods: {
         buscarAlumno(){
-            this.obtenerAlumno();
+            this.obtenerAlumnos(this.buscar);
         },
         guardarAlumno(){
             let sql = '',
-            parametros = [];
-        if(this.alumno.accion == 'nuevo'){
-            sql = 'INSERT INTO alumno (codigo, nombre, apellido, nacimiento, direccion, telefono, correo, dui) VALUES (?,?,?,?,?,?,?,?)';
-            parametros = [this.alumno.codigo, this.alumno.nombre, this.alumno.apellido, this.alumno.nacimiento, this.alumno.direccion,this.alumno.telefono, this.alumno.correo, this.alumno.dui];
-        }else if(this.alumno.accion == 'modificar'){
-            sql = 'UPDATE alumno SET codigo=?, nombre=?, apellido=?, nacimiento=?, direccion=?, telefono=?, correo=?, dui=? WHERE idCliente=?';
-            parametros = [this.alumno.codigo, this.alumno.nombre, this.alumno.apellido, this.alumno.nacimiento, this.alumno.direccion,this.alumno.telefono, this.alumno.correo, this.alumno.dui,this.alumno.idAlumno];
-        }else if(this.alumno.accion == 'eliminar'){
-            sql = 'DELETE FROM alumno WHERE idAlumno=?';
-            parametros = [this.alumno.idAlumno];
-        }
-        db_registro.transaction(tx=>{
-            tx.executeSql(sql,
-                parametros,
-            (tx, results)=>{
-                this.alumno.msg = 'Alumno procesado con exito';
-                this.nuevoAlumno();
-                this.obtenerAlumno();
-            },
-            (tx, error)=>{
-                this.alumno.msg = `Error al guardar el alumno ${error.message}`;
+                parametros = [];
+            if(this.alumno.accion == 'nuevo'){
+                sql = 'INSERT INTO alumnos (codigo, nombre, apellido, nacimiento, direccion, telefono, correo, dui) VALUES (?,?,?,?,?,?,?,?)';
+                parametros = [this.alumno.codigo,this.alumno.nombre,this.alumno.apellido,this.alumno.nacimiento,this.alumno.direccion,this.alumno.telefono,this.alumno.correo,this.alumno.dui];
+            }else if(this.alumno.accion == 'modificar'){
+                sql = 'UPDATE alumnos SET codigo=?, nombre=?, apellido=?, nacimiento=?, direccion=?, telefono=?, correo=?, dui=? WHERE idAlumno=?';
+                parametros = [this.alumno.codigo,this.alumno.nombre,this.alumno.apellido,this.alumno.nacimiento,this.alumno.direccion,this.alumno.telefono,this.alumno.correo,this.alumno.dui,this.alumno.idAlumno];
+            }else if(this.alumno.accion == 'eliminar'){
+                sql = 'DELETE FROM alumnos WHERE idAlumno=?';
+                parametros = [this.alumno.idAlumno];
+            }
+            db_registro.transaction(tx=>{
+                tx.executeSql(sql,
+                    parametros,
+                (tx, results)=>{
+                    this.alumno.msg = 'Alumno procesado con exito';
+                    this.nuevoAlumno();
+                    this.obtenerAlumnos();
+                },
+                (tx, error)=>{
+                    this.alumno.msg = `Error al guardar el alumno ${error.message}`;
+                });
             });
-        });
         },
         modificarAlumno(alumno){
             this.alumno = alumno;
             this.alumno.accion = 'modificar';
         },
         eliminarAlumno(alumno){
-            if( confirm(`¿Esta seguro de eliminar el Alumno ${alumno.nombre}?`)){
+            if( confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`) ){
                 this.alumno.idAlumno = alumno.idAlumno;
                 this.alumno.accion = 'eliminar';
                 this.guardarAlumno();
             }
         },
-        obtenerAlumno(busqueda=''){
+        obtenerAlumnos(busqueda=''){
             db_registro.transaction(tx=>{
-                tx.executeSql(`SELECT * FROM alumno WHERE codigo like "%${busqueda}%" OR nombre like "%${busqueda}%`, [], (tx, results)=>{
-                    this.alumno = results.rows;
+                tx.executeSql(`SELECT * FROM alumnos WHERE nombre like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
+                    this.alumnos = results.rows;
+                    /*this.clientes = [];
+                    for(let i=0; i<results.rows.length; i++){
+                        this.clientes.push(results.rows.item(i));
+                    }*/
                 });
             });
         },
-       
         nuevoAlumno(){
             this.alumno.accion = 'nuevo';
             this.alumno.idAlumno = '';
@@ -82,14 +85,14 @@ var app = new Vue({
             this.alumno.correo = '';
             this.alumno.dui = '';
         }
+        
     },
     created(){
         db_registro.transaction(tx=>{
-            tx.executeSql('CREATE TABLE IF NOT EXISTS alumno(idAlumno INTEGER PRIMARY KEY AUTOINCREMENT, '+
+            tx.executeSql('CREATE TABLE IF NOT EXISTS alumnos(idAlumno INTEGER PRIMARY KEY AUTOINCREMENT, '+
             'codigo char(10), nombre char(75), apellido char(75), nacimiento char(15), direccion TEXT, telefono char(10), correo char(40), dui char(10))');
         }, err=>{
             console.log('Error al crear la tabla alumno', err);
         });
-        this.obtenerAlumno();
     }
-});
+})
